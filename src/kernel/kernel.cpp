3851a1edaf719d94842301e6dbf1a022d9efd51d
BaileyBattles <bailey.battles@gmail.com>
2020-04-11 10:16:40 -0700

    Global constructors!

--- src/kernel/kernel.cpp
+++ src/kernel/kernel.cpp
@@ -8,6 +8,9 @@
 
 extern unsigned long ebss;
 
+Keyboard keyboard;
+
+
 void kernelLoop() {
     while (true)
     {}
@@ -18,15 +21,29 @@ void testInterrupts() {
     //__asm__ __volatile__("int $3");
 }
 
+typedef void (*constructor)();
+extern "C" constructor start_ctors;
+extern "C" constructor end_ctors;
+
+extern "C" void callConstructors()
+{
+    for(constructor* i = &start_ctors; i != &end_ctors; i++)
+        (*i)();
+}
+
 extern "C" void main(multiboot_header_t* multibootHeader) {
     clear_screen();
+
+    for(constructor* i = &start_ctors; i != &end_ctors; i++)
+        (*i)();
+    
     u32 endOfBss = (u32)&ebss;
+    KMemoryManager KMM(endOfBss);
+
     InterruptManager interruptManager;
-    Keyboard keyboard;
     keyboard.initialize();
 
     //Idealy don't want KMM on stack, should be in BSS I think
-    KMemoryManager KMM(endOfBss);
     char *buffer = (char*)KMM.kmalloc(32);
     if (buffer == nullptr){
         kprint("Failed to initialize buffer");
