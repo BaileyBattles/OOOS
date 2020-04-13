9d32c012ef71e418701e8894f8c0f12db3736a05
BaileyBattles <bailey.battles@gmail.com>
2020-04-13 15:53:53 -0700

    IO additions

--- include/sys/io.h
+++ include/sys/io.h
@@ -3,9 +3,11 @@
 
 #include "kernel/types.h"
 
-unsigned char inb(u16 port);
+u8 inb(u16 port);
 void outb(u16 port, u8 data);
-unsigned short inw(u16 port);
+u16 inw(u16 port);
 void outw(u16 port, u16 data);
+u32 inl(u16 port);
+void outl(u16 port, u32 data);
 
 #endif
--- src/sys/io.cpp
+++ src/sys/io.cpp
@@ -1,38 +1,33 @@
 #include "sys/io.h"
 #include "kernel/types.h"
 
-/**
- * Read a byte from the specified port
- */
-u8 inb(u16 port) {
-    u8 result;
-    /* Inline assembler syntax
-     * !! Notice how the source and destination registers are switched from NASM !!
-     *
-     * '"=a" (result)'; set '=' the C variable '(result)' to the value of register e'a'x
-     * '"d" (port)': map the C variable '(port)' into e'd'x register
-     *
-     * Inputs and outputs are separated by colons
-     */
-    __asm__("in %%dx, %%al" : "=a" (result) : "d" (port));
-    return result;
+u8 inb(u16 port)
+{
+	u8 ret;
+	asm volatile("inb %%dx, %%al":"=a"(ret):"d"(port));
+	return ret;
 }
-
-void outb(u16 port, u8 data) {
-    /* Notice how here both registers are mapped to C variables and
-     * nothing is returned, thus, no equals '=' in the asm syntax 
-     * However we see a comma since there are two variables in the input area
-     * and none in the 'return' area
-     */
-    __asm__ __volatile__("out %%al, %%dx" : : "a" (data), "d" (port));
+u16 inw(u16 port)
+{
+	u16 ret;
+	asm volatile("inw %%dx, %%ax":"=a"(ret):"d"(port));
+	return ret;
 }
-
-u16 inw(u16 port) {
-    u16 result;
-    __asm__("in %%dx, %%ax" : "=a" (result) : "d" (port));
-    return result;
+u32 inl(u16 port)
+{
+	u32 ret;
+	asm volatile("inl %%dx, %%eax":"=a"(ret):"d"(port));
+	return ret;
 }
-
-void outw(u16 port, u16 data) {
-    __asm__ __volatile__("out %%ax, %%dx" : : "a" (data), "d" (port));
+void outb(u16 port, u8 value)
+{
+	asm volatile("outb %%al, %%dx": :"d" (port), "a" (value));
+}
+void outw(u16 port, u16 value)
+{
+	asm volatile("outw %%ax, %%dx": :"d" (port), "a" (value));
+}
+void outl(u16 port, u32 value)
+{
+	asm volatile("outl %%eax, %%dx": :"d" (port), "a" (value));
 }
