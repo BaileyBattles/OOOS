22c9241ca2f72ae573ce3d9ce9197d8bc7b7f1b5
BaileyBattles <bailey.battles@gmail.com>
2020-04-19 15:13:36 -0700

    Write LIB Dir

--- scripts/writeRootFAT16.py
+++ scripts/writeRootFAT16.py
@@ -1,3 +1,4 @@
+import base64
 import os
 
 FAT_ENTRY_SIZE = 2 #bytes
@@ -10,16 +11,25 @@ DISK_NAME = os.path.join(os.getcwd(), 'drive/storage.img')
 
 ROOT_CLUSTER = FAT_START_CLUSTER + int(TOTAL_CLUSTERS * FAT_ENTRY_SIZE / CLUSTER_SIZE)
 
+def write_root_cluster(filename):
+    byteString = \
+    b'\x2e\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x10\x00\x00\x00\x00' + \
+    b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x2b\x00\x00\x00\x00\x00' + \
+    b'\x2e\x2e\x00\x00\x00\x00\x00\x00\x00\x00\x00\x10\x00\x00\x00\x00' + \
+    b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x2b\x00\x00\x00\x00\x00' + \
+    b'\x4C\x49\x42\x00\x00\x00\x00\x00\x00\x00\x00\x56\x00\x00\x00\x00' + \
+    b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x2c\x00\x00\x00\x00\x00'
+    with open(filename, 'r+b') as f:
+        f.seek((ROOT_CLUSTER) * SECTORS_PER_CLUSTER * SECTOR_SIZE)
+        f.write(byteString)
+
+
 def write_file(filename):
-    
-    with open(DISK_NAME) as disk:
-        with open(filename) as f:
-            content = f.read()
-            print(content)
     print(ROOT_CLUSTER)
 
 def main():
     filename = os.path.join(os.getcwd(), 'ex.txt')
+    write_root_cluster(DISK_NAME)
     write_file(filename)
 
 if __name__ == '__main__':
