6a754ed3387c0b656dfcc492fe6eeab0bf02ea93
BaileyBattles <bailey.battles@gmail.com>
2020-04-20 11:46:05 -0700

    Prints hellohello

--- src/Binutils/hello_world.cpp
+++ src/Binutils/hello_world.cpp
@@ -1,10 +1,6 @@
 #include "Drivers/Screen.h"
 
 int main() {
-    char *ptr = (char *)0x2000000;
-    ptr[0] = 'H';
-    ptr[1] = 'e';
-    ptr[2] = 'l';
     char data[] = "Hello";
     kprint(data);
 }
