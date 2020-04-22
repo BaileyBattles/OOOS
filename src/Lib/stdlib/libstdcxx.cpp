162433543e87828ea223419b5d0750276bfe39e9
BaileyBattles <bailey.battles@gmail.com>
2020-04-22 12:00:40 -0700

    Hacky fix to allow static objects

--- src/Lib/stdlib/libstdcxx.cpp
+++ src/Lib/stdlib/libstdcxx.cpp
@@ -1,8 +1,16 @@
 #include "Drivers/Screen.h"
 
+//Will need to implement C++ ABI someday
+
 extern "C"
 void __cxa_pure_virtual (void) {
     kprint("Pure virtual method called\n");
     while (true)
         ;
 }
+
+void *__dso_handle;
+
+extern "C"
+int __cxa_atexit(void (*func) (void *), void * arg, void * dso_handle) 
+{}
